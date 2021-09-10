43417c25411539e1a9f837ea3b5aa8635e59500a
Niels Rogge <niels.rogge1@gmail.com>
2021-09-10 11:54:00 +0200

    Move text preprocessor to separate script

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1049,6 +1049,7 @@ if is_torch_available():
             "PerceiverLayer",
             "PerceiverModel",
             "PerceiverPreTrainedModel",
+            "PerceiverTextPreprocessor",
         ]
     )
     _import_structure["models.prophetnet"].extend(
@@ -2767,6 +2768,7 @@ if TYPE_CHECKING:
             PerceiverLayer,
             PerceiverModel,
             PerceiverPreTrainedModel,
+            PerceiverTextPreprocessor,
         )
         from .models.prophetnet import (
             PROPHETNET_PRETRAINED_MODEL_ARCHIVE_LIST,
