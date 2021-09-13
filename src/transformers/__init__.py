ab0981bdb194269fa03203db613f041138bb2fb8
Niels Rogge <niels.rogge1@gmail.com>
2021-09-13 17:03:18 +0200

    Add PerceiverImagePreprocessor

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1047,6 +1047,7 @@ if is_torch_available():
             "PerceiverPreTrainedModel",
             "PerceiverTextPostprocessor",
             "PerceiverTextPreprocessor",
+            "PerceiverImagePreprocessor",
         ]
     )
     _import_structure["models.prophetnet"].extend(
@@ -2763,6 +2764,7 @@ if TYPE_CHECKING:
             PerceiverPreTrainedModel,
             PerceiverTextPostprocessor,
             PerceiverTextPreprocessor,
+            PerceiverImagePreprocessor,
         )
         from .models.prophetnet import (
             PROPHETNET_PRETRAINED_MODEL_ARCHIVE_LIST,
